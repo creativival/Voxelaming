a5714a8baf9cba57ec8f5808ddcfdcfb44c227d8
creativival <creativival@gmail.com>
2023-07-18 07:27:30 +0900

    ファイル名を統一するために変更

--- sample/swift/makeModel/Package.swift
+++ sample/swift/makeModel/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "model",
+    name: "makeModel",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "model",
+            name: "makeModel",
             path: "Sources",
             resources: [
                .process("piyo.ply"),
