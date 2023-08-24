889ff40d21d110c12ea0a7410e4f3f05c2327237
creativival <creativival@gmail.com>
2023-08-24 19:07:12 +0900

    フロートコマンドの実装 Swiftのコードを追加

--- sample/swift/rotateNode/Sources/BuildBox.swift
+++ sample/swift/rotateNode/Sources/BuildBox.swift
@@ -16,6 +16,7 @@ class BuildBox {
     var shape: String = "box"
     var isMetallic: Int = 0
     var roughness: Double = 0.5
+    var isAllowedFloat: Int = 0
     var buildInterval = 0.01
 
     init(roomName: String) {
@@ -24,42 +25,46 @@ class BuildBox {
     }
 
     func animateGlobal(_ x: Double, _ y: Double, _  z: Double, pitch: Double = 0, yaw: Double = 0, roll: Double = 0, scale: Double = 1, interval: Double = 10) {
-        let floorX = floor(x)
-        let floorY = floor(y)
-        let floorZ = floor(z)
-        globalAnimation = [floorX, floorY, floorZ, pitch, yaw, roll, scale, interval]
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
+        globalAnimation = [roundX, roundY, roundZ, pitch, yaw, roll, scale, interval]
     }
 
     func translate(_ x: Double, _ y: Double, _  z: Double, pitch: Double = 0, yaw: Double = 0, roll: Double = 0) {
-        let floorX = floor(x)
-        let floorY = floor(y)
-        let floorZ = floor(z)
-        node = [floorX, floorY, floorZ, pitch, yaw, roll]
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
+        node = [roundX, roundY, roundZ, pitch, yaw, roll]
     }
 
     func animate(_ x: Double, _ y: Double, _  z: Double, pitch: Double = 0, yaw: Double = 0, roll: Double = 0, scale: Double = 1, interval: Double = 10) {
-        clearData()
-        let floorX = floor(x)
-        let floorY = floor(y)
-        let floorZ = floor(z)
-        animation = [floorX, floorY, floorZ, pitch, yaw, roll, scale, interval]
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
+        animation = [roundX, roundY, roundZ, pitch, yaw, roll, scale, interval]
     }
 
     func createBox(_ x: Double, _ y: Double, _  z: Double, r: Double = 1, g: Double = 1, b: Double = 1, alpha: Double = 1) {
-        let floorX = floor(x)
-        let floorY = floor(y)
-        let floorZ = floor(z)
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
         // 重ねて置くことを防止するために、同じ座標の箱があれば削除する
-        removeBox(floorX, floorY, floorZ)
-        boxes.append([floorX, floorY, floorZ, r, g, b, alpha])
+        removeBox(roundX, roundY, roundZ)
+        boxes.append([roundX, roundY, roundZ, r, g, b, alpha])
     }
 
     func removeBox(_ x: Double, _ y: Double, _ z: Double) {
-        let floorX = floor(x)
-        let floorY = floor(y)
-        let floorZ = floor(z)
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
         for box in boxes {
-            if (box[0] == floorX && box[1] == floorY && box[2] == floorZ) {
+            if (box[0] == roundX && box[1] == roundY && box[2] == roundZ) {
                 boxes.remove(at: boxes.firstIndex(of: box)!)
             }
         }
@@ -85,13 +90,18 @@ class BuildBox {
         size = 1.0
         isMetallic = 0
         roughness = 0.5
+        isAllowedFloat = 0
         shape = "box"
     }
 
     func writeSentence(_ string_sentence: String, _ x: Double, _ y: Double, _  z: Double, r: Double = 0, g: Double = 0, b: Double = 0, alpha: Double = 1) {
-        let stringX = String(floor(x))
-        let stringY = String(floor(y))
-        let stringZ = String(floor(z))
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
+        let stringX = String(roundX)
+        let stringY = String(roundY)
+        let stringZ = String(roundZ)
         let stringR = String(r)
         let stringG = String(g)
         let stringB = String(b)
@@ -100,9 +110,10 @@ class BuildBox {
     }
 
     func setLight(_ x: Double, _ y: Double, _  z: Double, r: Double = 0, g: Double = 0, b: Double = 0, alpha: Double = 1, intensity: Double = 1000, interval: Double = 1, lightType: String = "point") {
-        let floorX = floor(x)
-        let floorY = floor(y)
-        let floorZ = floor(z)
+        let roundNumList = roundNumbers(numList: [x, y, z])
+        let roundX = roundNumList[0]
+        let roundY = roundNumList[1]
+        let roundZ = roundNumList[2]
         var doubleLightType: Double
 
         if lightType == "point" {
@@ -114,11 +125,15 @@ class BuildBox {
         } else {
             doubleLightType = 1
         }
-        lights.append([floorX, floorY, floorZ, r, g, b, alpha, intensity, interval, doubleLightType])
+        lights.append([roundX, roundY, roundZ, r, g, b, alpha, intensity, interval, doubleLightType])
     }
 
     func setCommand(_ command: String) {
         commands.append(command)
+
+        if command == "float" {
+            isAllowedFloat = 1
+        }
     }
 
     func drawLine(_ x1: Double, _ y1: Double, _ z1: Double, _ x2: Double, _ y2: Double, _ z2: Double, r: Double = 1, g: Double = 1, b: Double = 1, alpha: Double = 1) {
@@ -212,6 +227,7 @@ class BuildBox {
             "isMetallic": isMetallic,
             "roughness": roughness,
             "interval": buildInterval,
+            "isAllowedFloat": isAllowedFloat,
             "date": dateString
         ] as [String : Any]
 
@@ -234,6 +250,14 @@ class BuildBox {
             } catch {
                 print("An error occurred: \(error)")
             }
+        }
+    }
+
+    func roundNumbers(numList: [Double]) -> [Double] {
+        if isAllowedFloat == 1 {
+            return numList.map { round($0 * 100) / 100 }
+        } else {
+            return numList.map { floor($0) }
         }
     }
 }
