9571bd80dd117a3d43c60040a68e3a1cd2e23675
creativival <creativival@gmail.com>
2024-10-10 13:29:23 +0900

    VoxelammingパッケージをWebsocketの連続送信できるようにする

--- sample/swift/rotateNode/Sources/main.swift
+++ sample/swift/rotateNode/Sources/main.swift
@@ -11,29 +11,29 @@ if #available(iOS 15.0, macOS 12.0, *) {
                 [0, 30, 0],
                 [0, 0, 30]
             ]
-            let buildBox = BuildBox(roomName: roomName)
-            buildBox.setBoxSize(0.5)
-            buildBox.setBuildInterval(0.01)
+            let vox = Voxelamming(roomName: roomName)
+            vox.setBoxSize(0.5)
+            vox.setBuildInterval(0.01)
 
             for i in 0..<10 {
-                buildBox.createBox(-1, Double(i), 0, r: 0, g: 1, b: 1)
-                buildBox.createBox(0, Double(i), 0, r: 1, g: 0, b: 0)
-                buildBox.createBox(1, Double(i), 0, r: 1, g: 1, b: 0)
-                buildBox.createBox(2, Double(i), 0, r: 0, g: 1, b: 1)
+                vox.createBox(-1, Double(i), 0, r: 0, g: 1, b: 1)
+                vox.createBox(0, Double(i), 0, r: 1, g: 0, b: 0)
+                vox.createBox(1, Double(i), 0, r: 1, g: 1, b: 0)
+                vox.createBox(2, Double(i), 0, r: 0, g: 1, b: 1)
             }
 
             for i in 0..<5 {
-                buildBox.removeBox(0, Double(i * 2), 0)
-                buildBox.removeBox(1, Double(i * 2 + 1), 0)
+                vox.removeBox(0, Double(i * 2), 0)
+                vox.removeBox(1, Double(i * 2 + 1), 0)
             }
 
             for rotation in rotations {
               let pitch = Double(rotation[0])
               let yaw = Double(rotation[1])
               let roll = Double(rotation[2])
-              buildBox.transform(0, 0, 0, pitch: pitch, yaw: yaw , roll: roll)
-              try await buildBox.sendData()
-              sleep(1)
+              vox.transform(0, 0, 0, pitch: pitch, yaw: yaw , roll: roll)
+              try await vox.sendData()
+              usleep(100_000) // 0.1秒待機
             }
             // Edit code here.
         } catch {
