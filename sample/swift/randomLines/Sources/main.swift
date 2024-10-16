ef52e839e1dbd857e89cf62d68bbdd9adeee305d
creativival <creativival@gmail.com>
2024-10-16 18:02:33 +0900

    VoxelammingSwiftのrandomLinesサンプルを修正する

--- sample/swift/randomLines/Sources/main.swift
+++ sample/swift/randomLines/Sources/main.swift
@@ -11,12 +11,12 @@ if #available(iOS 15.0, macOS 12.0, *) {
             vox.transform(0, 30, 0, pitch: 0, yaw: 0, roll: 0)
 
             for _ in 0..<100 {
-                let x = CGFloat.random(in: -30...30)
-                let y = CGFloat.random(in: -30...30)
-                let z = CGFloat.random(in: -30...30)
-                let r = CGFloat.random(in: 0...1)
-                let g = CGFloat.random(in: 0...1)
-                let b = CGFloat.random(in: 0...1)
+                let x = Double.random(in: -30...30)
+                let y = Double.random(in: -30...30)
+                let z = Double.random(in: -30...30)
+                let r = Double.random(in: 0...1)
+                let g = Double.random(in: 0...1)
+                let b = Double.random(in: 0...1)
                 vox.drawLine(0, 0, 0, x, y, z, r: r, g: g, b: b, alpha: 1)
             }
 
